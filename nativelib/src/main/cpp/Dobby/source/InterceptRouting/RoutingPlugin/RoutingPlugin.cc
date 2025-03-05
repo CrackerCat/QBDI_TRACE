4546497dac1b0eed629fabcbba586b9434bc0926
xiaotui <nishixiaotujima@gmail.com>
2025-03-05 18:24:34 +0800

    dobby作为独立模块

---
+++ nativelib/src/main/cpp/Dobby/source/InterceptRouting/RoutingPlugin/RoutingPlugin.cc
@@ -0,0 +1,12 @@
+#include "InterceptRouting/RoutingPlugin/RoutingPlugin.h"
+
+tinystl::vector<RoutingPluginInterface *> RoutingPluginManager::plugins;
+
+RoutingPluginInterface *RoutingPluginManager::near_branch_trampoline = NULL;
+RoutingPluginInterface *RoutingPluginManager::QBDI_trampoline = NULL;
+
+void RoutingPluginManager::registerPlugin(const char *name, RoutingPluginInterface *plugin) {
+  DEBUG_LOG("register %s plugin", name);
+
+  RoutingPluginManager::plugins.push_back(plugin);
+}
